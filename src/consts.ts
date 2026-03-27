b1571722ef4e42d6f87f8fea826d5a2ec9cb40aa
Andy Xie <sxie4175@uni.sydney.edu.au>
2026-03-28 06:06:56 +1100

    changed description and cleaned up navbar

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Daily';
-export const SITE_DESCRIPTION = 'Daily entries';
+export const SITE_TITLE = 'andy xie';
+export const SITE_DESCRIPTION = 'blog';
